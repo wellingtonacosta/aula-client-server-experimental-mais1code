ae4db0f30dd76c172858d5719b564370019f2563
wellingtonacosta <wellingtonacosta09@gmail.com>
2023-02-28 17:56:17 -0300

    servidor pronto para producao

---
+++ src/index-node.js
@@ -0,0 +1,15 @@
+// const http = require("http")
+
+// const hostname = "localhost"
+// const port = 3333
+
+// const server = http.createServer((req, res) => {
+
+//   res.setHeader("Content-type", "text/plain")
+//   res.end("Hello World")
+
+// })
+
+// server.listen(port, hostname, () => {
+//   console.log('servidor rodando')
+// })
